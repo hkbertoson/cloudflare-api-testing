3d4d509ef5d7b68ab605be334ce153fadd221a03
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2025-05-16 12:58:27 -0500

    Updates

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,18 +1,18 @@
 export const prerender = false;
 import type {APIRoute} from 'astro';
 
-export const GET: APIRoute = () => {
-	return new Response(
-		JSON.stringify({
-			message: 'This is a GET Request',
-		}),
-		{
-			status: 200,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		}
-	);
+export const GET: APIRoute = async () => {
+	const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+	const buffer = await data.arrayBuffer();
+
+	console.log(buffer);
+
+	return new Response(buffer, {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
 };
 
 export const POST: APIRoute = () => {
